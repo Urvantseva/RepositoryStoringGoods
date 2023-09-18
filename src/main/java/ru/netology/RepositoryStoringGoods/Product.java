d26521d7910be066759a6e636ad20d5b987e4864
Julia <avokdorog@mail.ru>
2023-09-18 16:13:44 +0300

    Cheange

--- src/main/java/ru/netology/RepositoryStoringGoods/Product.java
+++ src/main/java/ru/netology/RepositoryStoringGoods/Product.java
@@ -26,6 +26,27 @@ public class Product {
     public int hashCode() {
         return Objects.hash(id, title, price);
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public int getId() {
         return id;
     }
